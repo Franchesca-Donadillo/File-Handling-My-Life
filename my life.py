1f34423b2e81f087d36b17dc263962540e2a3f78
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 18:23:03 +0800

    Working Program

--- my life.py
+++ my life.py
@@ -3,20 +3,18 @@
 # Prog 3 - My life
 
 # write a text file named mylife.txt using append
-with open("mylife.txt", "a") as file_life:
+with open("mylife.txt", "w") as file_life:
     
     #use while loop
-    i = 0
     yes_no = "y"
-    
-    user_input = input("Enter line: ")
-    ask_user = input("Are there more lines? y/n: ").lower()
-    
-    while True:
+        
+    while yes_no == "y":
+        # ask for user input
         user_input = input("Enter line: ")
-        file_life.write(user_input)
+        # write the user input into mylife.txt
+        file_life.write(user_input + "\n")
+        # ask user if they have more lines to add
         ask_user = input("Are there more lines? y/n: ").lower()
-        i+=1
 
         if ask_user == "n":
             print("Done!")
