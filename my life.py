c53e4b8ceb018ed37c301d6b729871cc0e725723
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-04-25 17:57:08 +0800

    loop working, file not

--- my life.py
+++ my life.py
@@ -4,20 +4,23 @@
 
 # write a text file named mylife.txt using append
 with open("mylife.txt", "a") as file_life:
-    # ask for user input
-    user_input = input("Enter line: ")
     
     #use while loop
     i = 0
-    yes_no = " "
-
-    while yes_no == " ":
+    yes_no = "y"
+    
+    user_input = input("Enter line: ")
+    ask_user = input("Are there more lines? y/n: ").lower()
+    
+    while True:
+        user_input = input("Enter line: ")
+        file_life.write(user_input)
         ask_user = input("Are there more lines? y/n: ").lower()
-        if yes_no =="y":
-            user_input = input("Enter line: ")
-            file_life.write(user_input)
-            i += 1
-        
-        elif yes_no == "n":
+        i+=1
+
+        if ask_user == "n":
             print("Done!")
             exit()
+        
+    
+        
